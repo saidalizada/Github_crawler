0b2c5a1fb46b4d1c33f7ff80a843d93059878eb6
saidalizada <saidalizada@gmail.com>
2020-12-24 13:22:31 +0100

    add Input json files

--- github_crawler.py
+++ github_crawler.py
@@ -3,7 +3,7 @@ from src.github_search import Github
 from src.utils.load_json import load_json
 
 if __name__ == "__main__":
-    _input = load_json('src\config\input.json')
+    _input = load_json('src\config\input_repositories.json')
     keywords = _input['keywords']
     proxies = _input['proxies']
     type_of_object = _input['type']
